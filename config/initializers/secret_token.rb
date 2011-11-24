d93c042ff3abeb7c695818aef1e9c1df4025c2ad
Maher Hawash <gmhawash@gmail.com>
2011-11-23 16:40:03 -0800

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Snapper::Application.config.secret_token = 'cafd5e4772f9d88af56964bba0c4c29ec4978fe06a42893f2a684c86bf8b0a661ef08f2c57e2fc451f83b4be3af6925c01c4b734f77ee2c5ffe9e142827afa66'
